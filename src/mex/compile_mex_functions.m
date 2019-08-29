53157a81d861d1da768b81f7410f6b505569788d
rogerhh <rogerhh@umich.edu>
2019-08-28 22:25:55 -0400

    fix mex compile error

--- src/mex/compile_mex_functions.m
+++ src/mex/compile_mex_functions.m
@@ -1,7 +1,7 @@
-mex -R2018a -lsqlite3 -lcurl makeConstraint.cpp ../DataPoint.cpp ../SunriseSunsetData.cpp ../WeatherData.cpp ../MBCFunctions.cpp;
+mex -R2018a -lsqlite3 -lcurl makeConstraint.cpp ../json11.cpp ../DataPoint.cpp ../SunriseSunsetData.cpp ../WeatherData.cpp ../MBCFunctions.cpp;
 mex andConstraint.cpp;
 mex orConstraint.cpp;
 mex notConstraint.cpp;
-mex -lsqlite3 -lcurl selectDatapoints.cpp ../DataPoint.cpp ../SunriseSunsetData.cpp ../WeatherData.cpp ../MBCFunctions.cpp
-mex -lsqlite3 -lcurl getSunData.cpp ../DataPoint.cpp ../SunriseSunsetData.cpp  ../WeatherData.cpp ../MBCFunctions.cpp
-mex -lsqlite3 -lcurl getWeatherData.cpp ../DataPoint.cpp ../SunriseSunsetData.cpp ../WeatherData.cpp ../MBCFunctions.cpp
+mex -lsqlite3 -lcurl selectDatapoints.cpp ../json11.cpp ../DataPoint.cpp ../SunriseSunsetData.cpp ../WeatherData.cpp ../MBCFunctions.cpp
+mex -lsqlite3 -lcurl getSunData.cpp ../json11.cpp ../DataPoint.cpp ../SunriseSunsetData.cpp  ../WeatherData.cpp ../MBCFunctions.cpp
+mex -lsqlite3 -lcurl getWeatherData.cpp ../json11.cpp ../DataPoint.cpp ../SunriseSunsetData.cpp ../WeatherData.cpp ../MBCFunctions.cpp

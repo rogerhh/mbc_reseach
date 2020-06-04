7686d926b50713ca8b7f4594e8059794705a963e
rogerhh <rogerhh@umich.edu>
2020-06-04 15:40:02 -0400

    v5.1 not compiled

--- src/mex/testscript.m
+++ src/mex/testscript.m
@@ -1,7 +1,7 @@
 % constr4 = makeConstraint('sn', '==', 20305333);
 % matrix = selectDatapoints(constr4);
 %v1 = getSunData('04/30/18', '04/31/18', 41.2936, -82.7233);
-v2 = getWeatherData('12/15/19', '12/16/19', 40, -84.2);
+v2 = getWeatherData('05/21/20', '05/26/20', 42.30, -83.72);
 %constr1 = makeConstraint('time', '>=', 1541116800);
 %constr2 = makeConstraint('time', '<', 1541203200);
 %v3 = selectDatapoints(andConstraint(constr1, constr2));
